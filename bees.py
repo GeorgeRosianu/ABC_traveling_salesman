40fe512580a4fe20e41bb2e564a47f271a68f610
GeorgeRosianu <georgerosianu12@gmail.com>
2021-06-24 18:37:20 +0300

    Removed Comments

--- bees.py
+++ bees.py
@@ -8,15 +8,12 @@ from scipy.spatial import distance
 class Bee:
     def __init__(self, node_set):
         self.role = ''
-        self.path = list(node_set)  # stores all nodes in each bee, will randomize foragers
+        self.path = list(node_set)
         self.distance = 0
-        self.cycle = 0  # number of iterations on current solution
+        self.cycle = 0
 
 
 def read_data_from_csv(file_name):
-    """
-    Returns data read from file.
-    """
     data_list = []
     with open(file_name) as f:
         reader = csv.reader(f)
@@ -25,16 +22,10 @@ def read_data_from_csv(file_name):
 
 
 def get_distance_between_nodes(n1, n2):
-    """
-    Calculates the Euclidean distance between two nodes.
-    """
     return distance.euclidean(n1, n2)
 
 
 def make_distance_table(data_list):
-    """
-    Creates a table that stores distance between every pair of nodes.
-    """
     length = len(data_list)
     table = [[get_distance_between_nodes(
         (data_list[i][1], data_list[i][2]), (data_list[j][1], data_list[j][2]))
@@ -43,13 +34,6 @@ def make_distance_table(data_list):
 
 
 def get_total_distance_of_path(path, table):
-    """
-    Calculates the total distance of an individual bee's path.
-    Terminates at starting node to complete cycle.
-    """
-    # Creates a copy of path, puts head at end of list.
-    # Zip lists to create pairs of neighbor coords,
-    # will create a cycle that terminates at starting node.
     new_path = list(path)
     new_path.insert(len(path), path[0])
     new_path = new_path[1:len(new_path)]
@@ -60,21 +44,12 @@ def get_total_distance_of_path(path, table):
 
 
 def initialize_hive(population, data):
-    """
-    Initializes a hive and populates it with bees.
-    Bees will have a randomized path attribute.
-    """
     path = [x[0] for x in data]
     hive = [Bee(path) for i in range(0, population)]
     return hive
 
 
 def assign_roles(hive, role_percentiles, table):
-    """
-    Assigns initial roles based on role percentiles
-    to each bee in the hive.
-    Assigns randomized path to forager bees.
-    """
     population = len(hive)
     onlooker_count = math.floor(population * role_percentiles[0])
     forager_count = math.floor(population * role_percentiles[1])
@@ -91,12 +66,6 @@ def assign_roles(hive, role_percentiles, table):
 
 
 def mutate_path(path):
-    """
-    Gets a random index 0 to next to last element.
-    Copies path, swaps two nodes, compares distance.
-    Returns mutated path.
-    """
-    # - will go out of range if last element is chosen.
     random_idx = random.randint(0, len(path) - 2)
     new_path = list(path)
     new_path[random_idx], new_path[random_idx + 1] = new_path[random_idx + 1], new_path[random_idx]
@@ -104,10 +73,6 @@ def mutate_path(path):
 
 
 def forage(bee, table, limit):
-    """
-    Worker bee behavior, iteratively refines a potential shortest path
-    by swapping randomly selected neighbor indices.
-    """
     new_path = mutate_path(bee.path)
     new_distance = get_total_distance_of_path(new_path, table)
 
@@ -123,10 +88,6 @@ def forage(bee, table, limit):
 
 
 def scout(bee, table):
-    """
-    Scout bee behavior, abandons unsuccessful path for new random path.
-    Resets role to forager.
-    """
     new_path = list(bee.path)
     random.shuffle(new_path)
     bee.path = new_path
@@ -136,11 +97,6 @@ def scout(bee, table):
 
 
 def waggle(hive, best_distance, table, forager_limit, scout_count):
-    """
-    Captures results from work of forager bees,
-    chooses new random path for scouts to explore,
-    returns results for overlookers to assess.
-    """
     best_path = []
     results = []
 
@@ -155,7 +111,6 @@ def waggle(hive, best_distance, table, forager_limit, scout_count):
         elif hive[i].role == 'S':
             scout(hive[i], table)
 
-    # after processing all bees, set worst performers to scout
     results.sort(reverse=True, key=lambda tup: tup[1])
     scouts = [tup[0] for tup in results[0:int(scout_count)]]
     for new_scout in scouts:
@@ -164,10 +119,6 @@ def waggle(hive, best_distance, table, forager_limit, scout_count):
 
 
 def recruit(hive, best_distance, best_path, table):
-    """
-    Recruits onlooker bees to iterate on best soluction so far.
-    Returns updated best_distance, best_path.
-    """
     for i in range(0, len(hive)):
         if hive[i].role == 'O':
             new_path = mutate_path(best_path)
@@ -178,19 +129,14 @@ def recruit(hive, best_distance, best_path, table):
     return best_distance, best_path
 
 
-def print_details(cycle, path, distance, bee):
-    """
-    Prints cycle details to console.
-    """
+def print_details(cycle, path, distance):
     print("CYCLE: {}".format(cycle))
     print("PATH: {}".format(path))
     print("DISTANCE: {}".format(distance))
-    # print("BEE: {}".format(bee))
     print("\n")
 
 
 def main():
-    # Control parameters
     population = 180
     forager_percent = 0.5
     onlooker_percent = 0.5
@@ -201,14 +147,11 @@ def main():
     cycle_limit = 2500
     cycle = 1
 
-    # Data source
-    data = read_data_from_csv("data/data_12.csv")
+    data = read_data_from_csv("data/data_15.csv")
 
-    # Global vars
     best_distance = sys.maxsize
     best_path = []
 
-    # Initialization
     table = make_distance_table(data)
     hive = initialize_hive(population, data)
     assign_roles(hive, role_percent, table)
@@ -218,15 +161,15 @@ def main():
         if waggle_distance < best_distance:
             best_distance = waggle_distance
             best_path = list(waggle_path)
-            print_details(cycle, best_path, best_distance, 'F')
-            result = (cycle, best_path, best_distance, 'F')
+            print_details(cycle, best_path, best_distance)
+            result = (cycle, best_path, best_distance)
 
         recruit_distance, recruit_path = recruit(hive, best_distance, best_path, table)
         if recruit_distance < best_distance:
             best_distance = recruit_distance
             best_path = list(recruit_path)
-            print_details(cycle, best_path, best_distance, 'R')
-            result = (cycle, best_path, best_distance, 'R')
+            print_details(cycle, best_path, best_distance)
+            result = (cycle, best_path, best_distance)
 
         cycle += 1
 
